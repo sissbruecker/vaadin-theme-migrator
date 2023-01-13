bd3f86b79e08d35b1702b6d697fbe2597246a25a
Sascha Ißbrücker <sissbruecker@vaadin.com>
2023-01-13 21:42:55 +0100

    Refactor to use postcss-selector-parser

--- src/plugins/part-to-slotted-element.js
+++ src/plugins/part-to-slotted-element.js
@@ -1,4 +1,5 @@
 const { Comment } = require("postcss");
+const parser = require("postcss-selector-parser");
 
 const configs = [
   {
@@ -8,57 +9,106 @@ const configs = [
   },
 ];
 
-function createTodo(rule, config) {
-  const text = `TODO: Automatic migration failed. [part='${config.partName}'] should be converted to ::slotted(${config.element})`;
-  const todo = new Comment({ text });
-  rule.root().insertBefore(rule, todo);
-}
+// ':host vaadin-avatar[part='avatar']:not(:first-child) .foo' -> 'vaadin-avatar[part='avatar']:not(:first-child)'
+function extractCompoundSelector(childNode) {
+  const sequence = [];
 
-function applyConfig(config, rule) {
-  const selectorRegex = new RegExp(`\\[part=['"]${config.partName}['"]\\]`);
-  if (!rule.selector.match(selectorRegex)) {
-    return;
+  // Find selector start
+  let currentNode = childNode;
+  while (currentNode.prev() && currentNode.prev().type !== "combinator") {
+    currentNode = currentNode.prev();
   }
 
-  const selectorList = rule.selector
-    .split(",")
-    .map((selector) => selector.trim());
+  // Collect nodes until next combinator, or end
+  while (currentNode && currentNode.type !== "combinator") {
+    sequence.push(currentNode);
+    currentNode = currentNode.next();
+  }
 
-  rule.selector = selectorList
-    .map((compoundSelector) => {
-      const startOfSequenceRegex = new RegExp(`^${selectorRegex.source}`, "g");
-      const childSelectors = compoundSelector
-        .split(" ")
-        .map((childSelector) => {
-          if (!childSelector.match(selectorRegex)) {
-            return childSelector;
-          }
-          // Converting wrapped selector is not supported
-          if (
-            childSelector.match(selectorRegex) &&
-            !childSelector.match(startOfSequenceRegex)
-          ) {
-            createTodo(rule, config);
-            return childSelector;
-          }
+  return sequence;
+}
 
-          const remainingSelector = childSelector.replace(
-            startOfSequenceRegex,
-            ""
-          );
-          return `::slotted(${config.element}${remainingSelector})`;
+function hasFollowingCombinator(childNode) {
+  let currentNode = childNode;
+
+  while (currentNode) {
+    if (currentNode.type === "combinator") {
+      return true;
+    }
+    currentNode = currentNode.next();
+  }
+
+  return false;
+}
+
+function createSelectorTransformer(config, rule) {
+  return (selectors) => {
+    const nodesToReplace = [];
+    let nodesToRemove = [];
+
+    selectors.walkAttributes((attributeNode) => {
+      if (
+        attributeNode.attribute === "part" &&
+        attributeNode.value === config.partName
+      ) {
+        // Extract full compound selector sequence that contains until next combinator
+        const compoundSelector = extractCompoundSelector(attributeNode);
+
+        // Create slotted selector with tag name
+        const slottedNode = parser.pseudo({ value: "::slotted" });
+        const slottedSelector = parser.selector();
+        const tagNode = parser.tag({ value: "vaadin-avatar" });
+        slottedNode.append(slottedSelector);
+        slottedSelector.append(tagNode);
+
+        // Clone compound selector into slotted selector
+        compoundSelector.forEach((node) => {
+          // Ignore attribute node
+          // Ignore tag nodes, makes no sense to combine them with the new slotted selector tag name
+          if (node !== attributeNode && node.type !== "tag") {
+            slottedSelector.append(node.clone());
+          }
         });
 
-      return childSelectors.join(" ");
-    })
-    .join(", ");
+        // Replace attribute selector with slotted selector
+        nodesToReplace.push([attributeNode, slottedNode]);
+
+        // Remove existing compound selector
+        nodesToRemove = [...nodesToRemove, ...compoundSelector];
+
+        // Output warning if there is a combinator after the new ::slotted selector,
+        // which is not valid CSS
+        if (hasFollowingCombinator(attributeNode)) {
+          createTodo(
+            rule,
+            config,
+            "Combining selectors after ::slotted() is not supported"
+          );
+        }
+      }
+    });
+
+    // Apply AST changes after traversal
+    // Doing this during traversal can result in errors
+    nodesToReplace.forEach((nodes) => nodes[0].replaceWith(nodes[1]));
+    nodesToRemove.forEach((node) => node.remove());
+  };
+}
+
+function createTodo(rule, config, text) {
+  const todo = new Comment({ text: `TODO: Migration issue: ${text}` });
+  rule.root().insertBefore(rule, todo);
 }
 
 module.exports = () => ({
   postcssPlugin: "vaadin-theme-migrator",
   Rule(rule) {
-    console.log(`Processing rule: ${rule.selector}`);
-    configs.forEach((config) => applyConfig(config, rule));
+    configs.forEach((config) => {
+      const transformer = createSelectorTransformer(config, rule);
+      const processor = parser(transformer);
+
+      rule.selector = processor.processSync(rule.selector);
+    });
   },
 });
 module.exports.postcss = true;
